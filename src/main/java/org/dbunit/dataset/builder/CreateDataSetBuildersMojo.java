86da9e29df4210d414010cca47cf6592b960b53c
alex-gab <dragoi.alex@gmail.com>
2016-10-19 22:51:15 +0300

    [#1]: fix code generation issue

--- src/main/java/org/dbunit/dataset/builder/CreateDataSetBuildersMojo.java
+++ src/main/java/org/dbunit/dataset/builder/CreateDataSetBuildersMojo.java
@@ -1,5 +1,8 @@
 package org.dbunit.dataset.builder;
 
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 import net.sf.jsqlparser.JSQLParserException;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
@@ -9,15 +12,22 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.dbunit.dataset.DataSetException;
 import org.dbunit.dataset.builder.javageneration.EntityClass;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PUBLIC;
+
 @Mojo(name = "generate-builders")
 public final class CreateDataSetBuildersMojo extends AbstractMojo {
-    @Parameter(property = "sayhi.file")
+    @Parameter(property = "schema.file")
     private File schemaFile;
 
     @Parameter(defaultValue = "${project}", readonly = true)
@@ -26,11 +36,10 @@ public final class CreateDataSetBuildersMojo extends AbstractMojo {
     @Override
     public final void execute() throws MojoExecutionException, MojoFailureException {
         try {
-            final List<EntityClass> entityClasses = parseSchema(schemaFile);
-            for (EntityClass entityClass : entityClasses) {
-                entityClass.generateCode();
-            }
-
+            final List<CreateTable> statements = parseSchema(schemaFile);
+            final String packageName = "org.dbunit.dataset.builder";
+            final List<EntityClass> entityClasses = createEntities(statements, packageName);
+            generateCode(entityClasses, packageName);
         } catch (SqlParsingException e) {
             throw new MojoExecutionException("Error parsing the sql file: " + e.getSchemaFileName(), e);
         } catch (CreateDataSetBuildersException e) {
@@ -38,18 +47,18 @@ public final class CreateDataSetBuildersMojo extends AbstractMojo {
         }
     }
 
-    private List<EntityClass> parseSchema(File schemaFile) throws SqlParsingException {
-        List<EntityClass> entityClasses = new ArrayList<>();
+    private List<CreateTable> parseSchema(File schemaFile) throws SqlParsingException {
+        List<CreateTable> createTables = new ArrayList<>();
         try {
             String[] sqlStatements = readSql(schemaFile);
             for (String sqlStatement : sqlStatements) {
                 CreateTable statement = (CreateTable) CCJSqlParserUtil.parse(new StringReader(sqlStatement));
-                entityClasses.add(new EntityClass(statement));
+                createTables.add(statement);
             }
         } catch (JSQLParserException | IOException e) {
             throw new SqlParsingException(e, schemaFile, "Could not parse sql schema");
         }
-        return entityClasses;
+        return createTables;
     }
 
     private String[] readSql(File schema) throws IOException {
@@ -63,5 +72,44 @@ public final class CreateDataSetBuildersMojo extends AbstractMojo {
         br.close();
         mysql = mysql.replaceAll("`", "");
         return mysql.split(";");
+    }
+
+    private List<EntityClass> createEntities(List<CreateTable> createTables, String packageName) {
+        List<EntityClass> entityClasses = new ArrayList<>();
+        for (CreateTable createTable : createTables) {
+            entityClasses.add(new EntityClass(createTable, packageName));
+        }
+        return entityClasses;
+    }
+
+    private void generateCode(List<EntityClass> entityClasses, String packageName) throws CreateDataSetBuildersException {
+        try {
+            Path generationPath = createGenerationFolder();
+
+            final TypeSpec.Builder dataSetBuilder = TypeSpec.classBuilder("SchemaDataSetBuilder").
+                    addModifiers(PUBLIC, FINAL).
+                    superclass(AbstractSchemaDataSetBuilder.class).
+                    addMethod(
+                            MethodSpec.constructorBuilder().
+                                    addModifiers(PUBLIC).
+                                    addException(DataSetException.class).
+                                    addStatement("super(new $T())", DataSetBuilder.class).
+                                    build()
+                    );
+            for (EntityClass entityClass : entityClasses) {
+                entityClass.addNewRowMethod(dataSetBuilder);
+                entityClass.generateRowBuilder(generationPath);
+            }
+
+            JavaFile.builder(packageName, dataSetBuilder.build()).build().writeTo(generationPath);
+        } catch (IOException e) {
+            throw new CreateDataSetBuildersException("Code generation exception: ", e);
+        }
+    }
+
+    private Path createGenerationFolder() throws IOException {
+        Path path = Paths.get("target/generated-test-sources/dbunit");
+        Files.createDirectories(path);
+        return path;
     }
 }
--- src/main/java/org/dbunit/dataset/builder/javageneration/EntityClass.java
+++ src/main/java/org/dbunit/dataset/builder/javageneration/EntityClass.java
@@ -3,95 +3,84 @@ package org.dbunit.dataset.builder.javageneration;
 import com.squareup.javapoet.*;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.builder.*;
+import org.dbunit.dataset.builder.AbstractSchemaDataRowBuilder;
+import org.dbunit.dataset.builder.ColumnSpec;
+import org.dbunit.dataset.builder.SqlTypes;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 import static javax.lang.model.element.Modifier.*;
 
 public final class EntityClass {
-    public static final String BUILDER_CLASS_PREFIX = "SchemaDataRowBuilder";
+    private static final String BUILDER_CLASS_PREFIX = "SchemaDataRowBuilder";
 
     private final CreateTable statement;
+    private final String packageName;
+    private final String tableName;
+    private final String rowBuilderClassName;
+    private final ClassName rowBuilderClass;
 
-    public EntityClass(CreateTable statement) {
+    public EntityClass(CreateTable statement, String packageName) {
         this.statement = statement;
+        this.packageName = packageName;
+        tableName = statement.getTable().getName();
+        rowBuilderClassName = tableName + BUILDER_CLASS_PREFIX;
+        rowBuilderClass = ClassName.get(packageName, rowBuilderClassName);
     }
 
-    public final void generateCode() throws CreateDataSetBuildersException {
-        try {
-            final String tableName = statement.getTable().getName();
-            final String rowBuilderClassName = tableName + BUILDER_CLASS_PREFIX;
-            final String packageName = "org.dbunit.dataset.builder";
-            final ClassName rowBuilderClass = ClassName.get(packageName, rowBuilderClassName);
+    public final void addNewRowMethod(TypeSpec.Builder dataSetBuilder) {
+        dataSetBuilder.
+                addMethod(
+                        MethodSpec.methodBuilder("new" + tableName + "Row").
+                                addModifiers(PUBLIC, FINAL).
+                                returns(rowBuilderClass).
+                                addStatement("return new $T($L, $S)", rowBuilderClass, "this", tableName).
+                                build()
+                );
+    }
 
-            final TypeSpec.Builder dataSetBuilder = TypeSpec.classBuilder("SchemaDataSetBuilder").
-                    addModifiers(PUBLIC, FINAL).
-                    superclass(AbstractSchemaDataSetBuilder.class).
+    public final void generateRowBuilder(Path generationPath) throws IOException {
+        final ClassName schemaDataSetBuilderClassName = ClassName.get(packageName, "SchemaDataSetBuilder");
+        ParameterizedTypeName superclass =
+                ParameterizedTypeName.get(
+                        ClassName.get(AbstractSchemaDataRowBuilder.class),
+                        schemaDataSetBuilderClassName);
+        final TypeSpec.Builder dataRowBuilder = TypeSpec.classBuilder(rowBuilderClassName).
+                addModifiers(PUBLIC, FINAL).
+                superclass(superclass).
+                addMethod(
+                        MethodSpec.constructorBuilder().
+                                addParameter(schemaDataSetBuilderClassName, "schemaDataSetBuilder", FINAL).
+                                addParameter(String.class, "tableName", FINAL).
+                                addStatement("super($L, $L)", "schemaDataSetBuilder", "tableName").
+                                build()
+                );
+
+        for (ColumnDefinition columnDefinition : statement.getColumnDefinitions()) {
+            final String dataType = columnDefinition.getColDataType().getDataType();
+            final String name = columnDefinition.getColumnName();
+            final Class clazz = SqlTypes.valueOf(dataType).getJavaClass();
+
+            TypeName columnSpec = ParameterizedTypeName.get(ColumnSpec.class, clazz);
+
+            final FieldSpec field = FieldSpec.builder(columnSpec, name, PRIVATE, STATIC, FINAL).
+                    initializer("ColumnSpec.newColumn($S)", name).
+                    build();
+            dataRowBuilder.
+                    addField(field).
                     addMethod(
-                            MethodSpec.constructorBuilder().
-                                    addModifiers(PUBLIC).
-                                    addException(DataSetException.class).
-                                    addStatement("super(new $T())", DataSetBuilder.class).
-                                    build()
-                    ).
-                    addMethod(
-                            MethodSpec.methodBuilder("new" + tableName + "Row").
+                            MethodSpec.methodBuilder(name).
                                     addModifiers(PUBLIC, FINAL).
                                     returns(rowBuilderClass).
-                                    addStatement("return new $T($L, $S)", rowBuilderClass, "this", tableName).
+                                    addParameter(clazz, name, FINAL).
+                                    addStatement("$L.with($L.$N, $L)", "dataRowBuilder", rowBuilderClassName, field, name).
+                                    addStatement("return this").
                                     build()
                     );
-
-            final ClassName schemaDataSetBuilderClassName = ClassName.get(packageName, "SchemaDataSetBuilder");
-            ParameterizedTypeName superclass =
-                    ParameterizedTypeName.get(
-                            ClassName.get(AbstractSchemaDataRowBuilder.class),
-                            schemaDataSetBuilderClassName);
-            final TypeSpec.Builder dataRowBuilder = TypeSpec.classBuilder(rowBuilderClassName).
-                    addModifiers(PUBLIC, FINAL).
-                    superclass(superclass).
-                    addMethod(
-                            MethodSpec.constructorBuilder().
-                                    addParameter(schemaDataSetBuilderClassName, "schemaDataSetBuilder", FINAL).
-                                    addParameter(String.class, "tableName", FINAL).
-                                    addStatement("super($L, $L)", "schemaDataSetBuilder", "tableName").
-                                    build()
-                    );
-
-            for (ColumnDefinition columnDefinition : statement.getColumnDefinitions()) {
-                final String dataType = columnDefinition.getColDataType().getDataType();
-                final String name = columnDefinition.getColumnName();
-                final Class clazz = SqlTypes.valueOf(dataType).getJavaClass();
-
-                TypeName columnSpec = ParameterizedTypeName.get(ColumnSpec.class, clazz);
-
-                final FieldSpec field = FieldSpec.builder(columnSpec, name, PRIVATE, STATIC, FINAL).
-                        initializer("ColumnSpec.newColumn($S)", name).
-                        build();
-                dataRowBuilder.
-                        addField(field).
-                        addMethod(
-                                MethodSpec.methodBuilder(name).
-                                        addModifiers(PUBLIC, FINAL).
-                                        returns(rowBuilderClass).
-                                        addParameter(clazz, name, FINAL).
-                                        addStatement("$L.with($L.$N, $L)", "dataRowBuilder", rowBuilderClassName, field, name).
-                                        addStatement("return this").
-                                        build()
-                        );
-            }
-
-            Path path = Paths.get("target/generated-test-sources/dbunit");
-            Files.createDirectories(path);
-            JavaFile.builder(packageName, dataSetBuilder.build()).build().writeTo(path);
-            JavaFile.builder(packageName, dataRowBuilder.build()).build().writeTo(path);
-        } catch (IOException e) {
-            throw new CreateDataSetBuildersException("Code generation exception: ", e);
         }
+
+
+        JavaFile.builder(packageName, dataRowBuilder.build()).build().writeTo(generationPath);
     }
 }
