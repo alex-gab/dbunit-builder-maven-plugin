91c11dca0ef770af4f59835ab772c8bd8f7c4ff3
alex-gab <dragoi.alex@gmail.com>
2016-10-20 22:15:46 +0300

    [#1]: use H2 data types

--- src/main/java/org/dbunit/dataset/builder/SqlTypes.java
+++ src/main/java/org/dbunit/dataset/builder/SqlTypes.java
@@ -1,11 +1,30 @@
 package org.dbunit.dataset.builder;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.Clob;
+import java.sql.Time;
 import java.util.Date;
+import java.util.UUID;
 
 public enum SqlTypes {
+    INT(Integer.class),
+    BOOOLEAN(Boolean.class),
+    TINYINT(Byte.class),
+    SMALLINT(Short.class),
+    BIGINT(Long.class),
+    IDENTITY(Long.class),
+    DECIMAL(BigDecimal.class),
+    DOUBLE(Double.class),
+    REAL(Float.class),
+    TIME(Time.class),
+    DATE(java.sql.Date.class),
+    TIMESTAMP(Date.class),
     VARCHAR(String.class),
-    DATE(Date.class),
-    NUMBER(Integer.class);
+    CHAR(String.class),
+    BLOB(Blob.class),
+    CLOB(Clob.class),
+    UUID(UUID.class);
 
     private final Class javaClass;
 
