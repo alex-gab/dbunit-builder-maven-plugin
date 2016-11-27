c586dafd9112bb2209567b8737645ef3976b0ba3
alex-gab <dragoi.alex@gmail.com>
2016-11-27 19:55:00 +0200

    [#1]: revamping project using builder pattern

--- src/main/java/org/dbunit/builder/ISchemaDataSetBuilder.java
+++
@@ -1,8 +0,0 @@
-package org.dbunit.builder;
-
-import org.dbunit.dataset.DataSetException;
-import org.dbunit.dataset.IDataSet;
-
-interface ISchemaDataSetBuilder {
-    IDataSet build() throws DataSetException;
-}
